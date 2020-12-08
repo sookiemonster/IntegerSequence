29ebba21063716697a01aac343d071f2be1cf399
sookiemonster <dsooknanan20@stuy.edu>
2020-12-08 11:47:07 -0500

    Fixed ArraySSequence constructor (copied values from other array instead of setting the data equal to the other array.)

--- ArraySequence.java
+++ ArraySequence.java
@@ -6,7 +6,14 @@ public class ArraySequence implements IntegerSequence {
 
   public ArraySequence(int[] other) {
     this.currentIndex = 0;
-    this.data = other;
+    this.data = new int[other.length];
+    for (int i = 0; i < data.length; i++) {
+      data[i] = other[i];
+    }
+  }
+
+  public ArraySequence(IntegerSequence other) {
+
   }
 
   public void reset() {
