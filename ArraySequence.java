aa1ad9f2bb908142cb043e9dd63b369045581031
sookiemonster <dsooknanan20@stuy.edu>
2020-12-08 11:28:47 -0500

    Added ArraySequence constructor.

--- ArraySequence.java
+++ ArraySequence.java
@@ -1,4 +1,11 @@
 import java.util.NoSuchElementException;
 
-public class ArraySequence {
+public class ArraySequence implements IntegerSequence {
+  private int currentIndex;
+  private int[] data;
+
+  public ArraySequence(int[] other) {
+    this.currentIndex = 0;
+    this.data = other;
+  }
 }
